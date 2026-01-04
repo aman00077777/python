9172165d690db34614fb6ee2ee9cb3b1817214d4
Aman <aman20061203@gmail.com>
2026-01-04 16:03:09 +0530

    py commit

---
+++ Chapter 1/codeExecution.py
@@ -0,0 +1,7 @@
+name = "Aman Sharma"
+age1 = 25
+age2 = 30
+print(age2)
+city = "New York"
+age2 = age1
+print(age2)
---
+++ Chapter 1/userInput.py
@@ -0,0 +1,5 @@
+#Program to take input from user and display it
+
+name = input("Enter your name: ")
+
+print("Hello, " + name + "! Welcome to the program.")
